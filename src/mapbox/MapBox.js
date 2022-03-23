8b15fb611d3eff2921e4a84373b597c858085ed5
CaliforniaLuv <caifornialove.96@gmail.com>
2022-03-24 00:52:40 +0900

    add: Layer typing

--- src/mapbox/MapBox.js
+++ src/mapbox/MapBox.js
@@ -1,32 +1,47 @@
 import './MapBox.css'
-import React, {useState} from 'react';
-import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import React, {useState, useCallback} from 'react';
+import ReactMapGL, { Layer, Marker, Popup, Source } from 'react-map-gl';
 import data from "../data/sanfrancisco.json"
+import {countiesLayer} from './map-style'
+
 
 function MapBox() {
     
-    const [viewport, setViewport] = useState({
-        latitude: 37.778259,
-        longitude: -122.417931,
-        width: '100vw',
-        height: '100vh',
-        zoom: 11,
-    })
+    const [hoverInfo, setHoverInfo] = useState(null);
 
+    // console.log(data.features[0].layer.id)
     const [name, setName] = useState('')
     const handleBtnClick = (value) => {
         setName(value)
     }
 
+    const onHover = useCallback((value) => {
+        console.log(value)
+    }, [])
+    
     return ( 
         <>
             <div style={{ height:"700px"}}>
                 <ReactMapGL
-                    {...viewport}
+                    initialViewState={{
+                        latitude: 37.778259,
+                        longitude: -122.417931,
+                        width: '100vw',
+                        height: '100vh',
+                        zoom: 11,
+                    }}
                     mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
                     mapStyle="mapbox://styles/californialuv/cl0ywdh8t000f14ocnrgsob9l"
-                    onMove={newport => setViewport(newport)}
-                >
+                    onMouseMove={onHover}
+                    interactiveLayerIds={['sanfrancisco']}
+                    
+                >  
+                    
+                      <Layer {...data.features.map((el) => {
+                          return el.layer
+                      })}/>
+                    
+                   
                     {
                         data.features.map((place) => (
                             <Marker
@@ -40,8 +55,6 @@ function MapBox() {
                                         <img className="marker_Total" src="/circle.png"/>
                                     }
                                 </button>
-                                
-
                             </Marker>
                         ))
                     }
---
+++ src/mapbox/map-style.js
@@ -0,0 +1,12 @@
+// import type {FillLayer} from 'react-map-gl';
+
+export const countiesLayer = {
+  id: 'sanfrancisco',
+  type: 'fill',
+  'source-layer': 'original',
+  paint: {
+    'fill-outline-color': 'rgba(0,0,0,0.1)',
+    'fill-color': 'rgba(0,0,0,0.1)'
+  }
+
+};
